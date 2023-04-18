8ec6dbb415be891d1f9999ff3c66813345431bb6
aldogerard <aldogerard12@gmail.com>
2023-04-19 02:09:14 +0700

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins"],
+      },
+      colors: {
+        primary: "#06b6d4",
+        secondary: "#64748b",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
